731c356ebbd294fd7384ce31b5ecf103b92a4921
Cassandre Tirions <ctirions@mi-r2-p4.s19.be>
2021-01-24 13:28:11 +0100

    Norme update

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/07 19:31:55 by ctirions          #+#    #+#             */
-/*   Updated: 2021/01/21 17:59:40 by ctirions         ###   ########.fr       */
+/*   Created: 2021/01/24 13:26:07 by ctirions          #+#    #+#             */
+/*   Updated: 2021/01/24 13:26:47 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 #include <unistd.h>
 
 int	main(void)
-{	
-	int d = -27;
-	int	*i = &d;
+{
+	int d;
+	int	*i;
 
+	d = -27;
+	i = &d;
 	printf("%d\n", printf("|%0*p|\n", -7, i));
 	printf("%d\n", ft_printf("|%0*p|\n", -7, i));
 	return (0);
